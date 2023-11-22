4b94b24d8dbd23b798336654736cde0fac1ea48d
jake <C92i747A@pm.me>
2023-11-22 11:44:41 +0000

    Added bundle time to build script

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import tinyBundle
+from timeit import default_timer as timer
 
 """
 This is the boilerplate for a bundle script, this uses the tinyBundle module to bundle all files
@@ -14,4 +15,7 @@ tinyBundle.run("out/bundle.py")
 
 files = ["src/__main__.py"] # Add other files here (can also be a list but a tuple if preferred)
 
+start = timer()
 tinyBundle.bundle(files,"out/",0) # out/ is the default output location and 0 is the default compression level
+end = timer()
+print("Bundle time = " + str(end - start) + " seconds") # time in seconds
